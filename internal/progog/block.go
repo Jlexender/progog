212794d2a6499840318bdeeb84257aa9a1d92f62
Alexander Kaliev <aleksandrdmitryevich@gmail.com>
2025-09-21 19:47:54 +0300

    added more block data

--- internal/progog/block.go
+++ internal/progog/block.go
@@ -6,6 +6,12 @@ type Block struct {
 	Version    int      `json:"ver"`
 	MerkleRoot string   `json:"mrkl_root"`
 	Nonce      uint32   `json:"nonce"`
+	Time       uint32   `json:"time"`
+	NTx        int      `json:"n_tx"`
+	Size       int      `json:"size"`
+	Height     int      `json:"height"`
+	Weight     int      `json:"weight"`
+	PrevBlock  string   `json:"prev_block"`
 }
 
 var BlockPool []*Block
