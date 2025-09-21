988da32ef016f2a46d7ad2bf2c7eeb98ff2eca2b
Alexander Kaliev <aleksandrdmitryevich@gmail.com>
2025-09-21 18:54:25 +0300

    update CLI

--- cmd/progog/main.go
+++ cmd/progog/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"jex/progog/internal/progog"
+	"os"
+)
 
 func main() {
-	println("Hello, World!")
+	startHash := flag.String("start", "", "Starting block hash")
+	depth := flag.Int("depth", 1, "Depth of traversal")
+	flag.Parse()
+	
+	if *startHash == "" || *depth < 1 {
+		throwExit()
+	}
+	
+	progog.Start(*startHash, *depth)
 }
+
+
+func throwExit() {
+	fmt.Println("Usage: progog -start <block_hash> -depth <depth>")
+	os.Exit(1)
+}
+
